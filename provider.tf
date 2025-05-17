8dd97e71d00bcdff7d89d50b5e7bf87425826ae8
Himanshu tiwari <68557180+HIMANSHUTIWARI14071999@users.noreply.github.com>
2025-05-17 19:18:46 +0530

    Create provider.tf
    new files added

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.28.0"
+    }
+  }
+}
+
+provider "azurerm" {
+   features {}
+   subscription_id = "211efec7-7c3e-48c1-8700-c98598950861"
+   client_id = "64b35fd8-fd07-4623-9ae0-b74f932518b3"
+   tenant_id = 
+   client_secret = 
+
+}
